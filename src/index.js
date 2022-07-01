06d74c85822059e7865df17221791fbcde385332
Jonathan Lu <106041063+jlucoding@users.noreply.github.com>
2022-07-01 10:34:56 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+// import  Constructor  from './template.js';
+
+
+$(document).ready(function() {
+  $('#form').submit(function(event) {
+    event.preventDefault();
+    
+  });
+});
