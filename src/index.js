0c7a90f55583afda2d48f84257dd1128c28234c2
Jonathan Lu <jdlu.coding@gmail.com>
2022-07-01 15:58:23 -0700

    fix module error

--- src/index.js
+++ src/index.js
@@ -4,10 +4,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import  ExchangeRateService  from './services/exchange-rate-service.js';
 
+function clearFields() {
+  $("#amountUSD").val("");
+  $("#desiredCurrency").val("");
+  $('.showErrors').html("");
+}
+
+function displayDesiredCurrency(desiredCurrency){
+  $(".showDesiredCurrency").html(`The exchange rate from ${amountUSD} USD to the desired currency is ${desiredCurrency}`);
+}
+
+function displayErrors(error) {
+  $('.showErrors').text(`${error}`);
+}
+
 
 $(document).ready(function() {
-  $('#form').submit(function(event) {
-    event.preventDefault();
-    
+  $('#amountUSD').click(function() {
+    let amountUSD = $("#amountUSD").val();
+    let desiredCurrency = $("desiredCurrency").val();
+    clearFields();
+    ExchangeRateService.getDesiredCurrency(amountUSD)
+      .then(function(response){
+        if (response instanceof Error) {
+          throw Error (`${response.message}`);
+        }
+        const desiredCurrencyResponse = response.conversion_rates[`${desiredCurrency}`];
+        displayDesiredCurrency(desiredCurrencyResponse);
+      })
+      .catch(function(error){
+        displayErrors(error.message);
+      });
   });
 });
